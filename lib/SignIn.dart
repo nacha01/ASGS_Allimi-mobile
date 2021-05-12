c37333a091e60550158bb88dfb34afa0679b2ece
SeungGun <saverrw2864@naver.com>
2021-05-12 17:44:34 +0900

    자동 로그인 시, 배경 어두움 + 로딩구현

--- lib/SignIn.dart
+++ lib/SignIn.dart
@@ -18,6 +18,7 @@ class _SignInPageState extends State<SignInPage> {
   String email = '';
   String password = '';
   bool _isChecked = false;
+  bool _logging = false;
   TextEditingController _emailController = TextEditingController();
   TextEditingController _passwordController = TextEditingController();
   SharedPreferences _pref;
@@ -29,30 +30,31 @@ class _SignInPageState extends State<SignInPage> {
     // _passwordController.text = '12345678';
   }
 
-  _loadLoginInfo() async{
+  _loadLoginInfo() async {
     _pref = await SharedPreferences.getInstance();
     setState(() {
       _isChecked = _pref.getBool('checked') ?? false;
-      if(_isChecked) {
-        _emailController.text =
-            _pref.getString('email') ?? '';
-        _passwordController.text =
-            _pref.getString('password') ?? '';
+      if (_isChecked) {
+        _emailController.text = _pref.getString('email') ?? '';
+        _passwordController.text = _pref.getString('password') ?? '';
         _emailSignIn();
         FirebaseAuth.instance.onAuthStateChanged.listen((fu) {
           Navigator.pushReplacement(context,
               MaterialPageRoute(builder: (context) {
-                return HomePage(
-                  user: fu,
-                  books: widget.books,
-                );
-              }));
+            return HomePage(
+              user: fu,
+              books: widget.books,
+            );
+          }));
         });
       }
     });
   }
 
   Future<FirebaseUser> _emailSignIn() async {
+    setState(() {
+      _logging = true;
+    });
     var email = _emailController.text ?? "";
     var pw = _passwordController.text ?? "";
     AuthResult result;
@@ -64,7 +66,7 @@ class _SignInPageState extends State<SignInPage> {
       return null;
     }
     print("signed in "); //firebase 로그인 완료
-
+    Future.delayed(Duration(seconds: 2));
     Firestore.instance
         .collection("users")
         .document(result.user.uid)
@@ -78,7 +80,9 @@ class _SignInPageState extends State<SignInPage> {
         print('Create data');
       }
     });
-
+    setState(() {
+      _logging = false;
+    });
     return result.user;
   }
 
@@ -90,138 +94,163 @@ class _SignInPageState extends State<SignInPage> {
           centerTitle: true,
           title: Text('Sign In Page'),
         ),
-        body: SingleChildScrollView(
-          child: Center(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Text(
-                  '이메일 방식으로 로그인하기',
-                  style: TextStyle(fontWeight: FontWeight.bold, fontSize: 25),
-                ),
-                SizedBox(height: 50.0),
-                Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: TextFormField(
-                    cursorColor: Colors.black,
-                    controller: _emailController,
-                    style: TextStyle(fontSize: 18.0, color: Colors.black),
-                    decoration: InputDecoration(
-                      fillColor: Colors.orange.withOpacity(0.1),
-                      filled: true,
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(20.0))),
-                      prefixIcon: Icon(Icons.account_circle),
-                      labelText: 'Email',
-                      labelStyle: TextStyle(
-                        fontSize: 16.0,
+        body: Container(
+          color: _logging ? Colors.grey[700] : Color(0x00000000),
+          child: Stack(
+            children: [
+              Center(
+                child: _logging
+                    ? CircularProgressIndicator()
+                    : Stack(),
+              ),
+              SingleChildScrollView(
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      Text(
+                        '이메일 방식으로 로그인하기',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 25),
                       ),
-                    ),
-                    onChanged: (value) {
-                      setState(() {
-                        email = value;
-                      });
-                    },
-                  ),
-                ),
-                SizedBox(height: 10.0),
-                Padding(
-                  padding: EdgeInsets.all(10.0),
-                  child: TextFormField(
-                    controller: _passwordController,
-                    obscureText: true,
-                    cursorColor: Colors.black,
-                    style: TextStyle(fontSize: 18.0, color: Colors.black),
-                    decoration: InputDecoration(
-                      fillColor: Colors.orange.withOpacity(0.1),
-                      filled: true,
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(20.0))),
-                      prefixIcon: Icon(Icons.vpn_key),
-                      labelText: 'Password',
-                      labelStyle: TextStyle(
-                        fontSize: 16.0,
+                      SizedBox(height: 50.0),
+                      Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: TextFormField(
+                          cursorColor: Colors.black,
+                          controller: _emailController,
+                          style: TextStyle(fontSize: 18.0, color: Colors.black),
+                          decoration: InputDecoration(
+                            fillColor: Colors.orange.withOpacity(0.1),
+                            filled: true,
+                            border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20.0))),
+                            prefixIcon: Icon(Icons.account_circle),
+                            labelText: 'Email',
+                            labelStyle: TextStyle(
+                              fontSize: 16.0,
+                            ),
+                          ),
+                          onChanged: (value) {
+                            setState(() {
+                              email = value;
+                            });
+                          },
+                        ),
                       ),
-                    ),
-                    onChanged: (value) {
-                      setState(() {
-                        password = value;
-                      });
-                    },
-                  ),
-                ),
-                Row(
-                  children: [
-                    Checkbox(value: _isChecked, onChanged: (value){
-                      setState(() {
-                        _isChecked = value;
-                      });
-                    }),
-                    Text('자동 로그인')
-                  ],
-                ),
-                SizedBox(height: 10.0),
-                RaisedButton(
-                  /////////
-                  onPressed: () async {
-                    if (_emailController.text.isEmpty ||
-                        _passwordController.text.isEmpty) {
-                      showDialog(
-                          context: context,
-                          builder: (context) {
-                            return AlertDialog(
-                              content: Text('내용을 입력하세요'),
-                              actions: [
-                                FlatButton(
-                                    onPressed: () => Navigator.pop(context),
-                                    child: Text('확인'))
-                              ],
-                            );
-                          });
-                      return;
-                    }
-                    _pref.setString(
-                        'email', _emailController.text.toString());
-                    _pref.setString(
-                        'password', _passwordController.text.toString());
-                    _pref.setBool('checked', _isChecked);
-                    try {
-                      setState(() {
-                        _loading = true;
-                      });
-                      await _emailSignIn();
-                      FirebaseAuth.instance.onAuthStateChanged.listen((fu) {
-                        Navigator.pushReplacement(context,
-                            MaterialPageRoute(builder: (context) {
-                          return HomePage(
-                            user: fu,
-                            books: widget.books,
+                      SizedBox(height: 10.0),
+                      Padding(
+                        padding: EdgeInsets.all(10.0),
+                        child: TextFormField(
+                          controller: _passwordController,
+                          obscureText: true,
+                          cursorColor: Colors.black,
+                          style: TextStyle(fontSize: 18.0, color: Colors.black),
+                          decoration: InputDecoration(
+                            fillColor: Colors.orange.withOpacity(0.1),
+                            filled: true,
+                            border: OutlineInputBorder(
+                                borderRadius:
+                                    BorderRadius.all(Radius.circular(20.0))),
+                            prefixIcon: Icon(Icons.vpn_key),
+                            labelText: 'Password',
+                            labelStyle: TextStyle(
+                              fontSize: 16.0,
+                            ),
+                          ),
+                          onChanged: (value) {
+                            setState(() {
+                              password = value;
+                            });
+                          },
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          Checkbox(
+                              value: _isChecked,
+                              onChanged: (value) {
+                                setState(() {
+                                  _isChecked = value;
+                                });
+                              }),
+                          Text('자동 로그인')
+                        ],
+                      ),
+                      SizedBox(height: 10.0),
+                      RaisedButton(
+                        /////////
+                        onPressed: () async {
+                          if (_emailController.text.isEmpty ||
+                              _passwordController.text.isEmpty) {
+                            showDialog(
+                                context: context,
+                                builder: (context) {
+                                  return AlertDialog(
+                                    content: Text('내용을 입력하세요'),
+                                    actions: [
+                                      FlatButton(
+                                          onPressed: () =>
+                                              Navigator.pop(context),
+                                          child: Text('확인'))
+                                    ],
+                                  );
+                                });
+                            return;
+                          }
+                          _pref.setString(
+                              'email', _emailController.text.toString());
+                          _pref.setString(
+                              'password', _passwordController.text.toString());
+                          _pref.setBool('checked', _isChecked);
+                          try {
+                            setState(() {
+                              _loading = true;
+                            });
+                            await _emailSignIn();
+                            FirebaseAuth.instance.onAuthStateChanged
+                                .listen((fu) {
+                              Navigator.pushReplacement(context,
+                                  MaterialPageRoute(builder: (context) {
+                                return HomePage(
+                                  user: fu,
+                                  books: widget.books,
+                                );
+                              }));
+                            });
+                            _loading = false;
+                          } catch (e) {
+                            print(e.toString());
+                          }
+                        },
+                        color: Colors.orangeAccent,
+                        child: Text('로그인 하기', style: TextStyle(fontSize: 17.0)),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0)),
+                      ),
+                      SizedBox(height: 20.0),
+                      RaisedButton(
+                        onPressed: () {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                                builder: (context) => SignUpPage(
+                                      books: widget.books,
+                                    )),
                           );
-                        }));
-                      });
-                      _loading = false;
-                    } catch (e) {
-                      print(e.toString());
-                    }
-                  },
-                  color: Colors.orangeAccent,
-                  child: Text('로그인 하기', style: TextStyle(fontSize: 17.0)),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0)),
+                        },
+                        color: Colors.orangeAccent,
+                        child: Text('이메일 방식으로 가입하기 ',
+                            style: TextStyle(fontSize: 17.0)),
+                        shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(20.0)),
+                      ),
+                    ],
+                  ),
                 ),
-                SizedBox(height: 20.0),
-                RaisedButton(
-                  onPressed: () {
-                    Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => SignUpPage(books: widget.books,)),
-                        );
-                  },
-                  color: Colors.orangeAccent,
-                  child: Text('이메일 방식으로 가입하기 ', style: TextStyle(fontSize: 17.0)),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0)),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ));
   }
