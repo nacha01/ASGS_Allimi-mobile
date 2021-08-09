8db5c443dff38132be7cafb9c8119907994fba33
SeungGun <saverrw2864@naver.com>
2021-08-10 00:29:04 +0900

    GridView로 아이템 나열 및 Item Layout 완성

--- lib/StoreMainPage.dart
+++ lib/StoreMainPage.dart
@@ -39,7 +39,7 @@ class _StoreMainPageState extends State<StoreMainPage>
               centerTitle: true,
               title: aboveTap(size),
               leading: Container(
-                margin: EdgeInsets.only(left: 9),
+                  margin: EdgeInsets.only(left: 9),
                   alignment: Alignment.center,
                   child: Text(
                     '나래',
@@ -75,8 +75,20 @@ class _StoreMainPageState extends State<StoreMainPage>
           ];
         },
         body: TabBarView(controller: _tabController, children: [
-          Container(
-            color: Colors.red,
+          GridView.count(padding: EdgeInsets.all(10),
+            crossAxisSpacing: 3,
+            mainAxisSpacing: 20,
+            crossAxisCount: 2,
+            children: [
+              itemTile('http://nacha01.dothome.co.kr/sin/snack3.jpg', '1,500',
+                  '서울 우유', false),
+              itemTile('http://nacha01.dothome.co.kr/sin/snack3.jpg', '3,000',
+                  '트롤리 젤리', true),
+              itemTile('http://nacha01.dothome.co.kr/sin/snack3.jpg', '1,500',
+                  '서울 우유2', false),
+              itemTile('http://nacha01.dothome.co.kr/sin/snack3.jpg', '3,000',
+                  '트롤리 젤리2', true)
+            ],
           ),
           Container(
             color: Colors.black,
@@ -105,17 +117,43 @@ class _StoreMainPageState extends State<StoreMainPage>
             ),
             label: '홈',
           ),
+          BottomNavigationBarItem(icon: Icon(Icons.alarm_rounded), label: '알림'),
           BottomNavigationBarItem(
-              icon: Icon(Icons.alarm_rounded),
-              label: '알림'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.shopping_cart),
-              label: '장바구니'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.person), label: '마이페이지')
+              icon: Icon(Icons.shopping_cart), label: '장바구니'),
+          BottomNavigationBarItem(icon: Icon(Icons.person), label: '마이페이지')
         ],
       ),
     );
+  }
+
+  Widget itemTile(String imgUrl, String price, String prodName, bool isWish) {
+    return GestureDetector(
+      onTap: (){
+        print('클릭함');
+      },
+      child: Column(
+        children: [
+          Expanded( //핵심.... Stack의 높이는 정해져있지 않아서 Expanded로?..
+            child: Stack(
+              children: [
+                ClipRRect(
+                  child: Image.network(
+                    imgUrl,
+                    fit: BoxFit.fill,
+                  ),
+                  borderRadius: BorderRadius.circular(15),
+                ),
+                Positioned(bottom: 8,right: 8,
+                    child: Icon(isWish ? Icons.favorite : Icons.favorite_border))
+              ],
+            ),
+          ),
+          Text(price),
+          Text(prodName)
+        ],
+      ),
+    );
+
   }
 
   Widget aboveTap(Size size) {
@@ -124,16 +162,19 @@ class _StoreMainPageState extends State<StoreMainPage>
       width: size.width * 0.78,
       decoration: BoxDecoration(
           borderRadius: BorderRadius.circular(12), color: Color(0xFF9EE1E5)),
-      child: TextField(onSubmitted: (text){ //완료 버튼 눌렀을 때
-        print(text);
-      },
+      child: TextField(
+        onSubmitted: (text) {
+          //완료 버튼 눌렀을 때
+          print(text);
+        },
         decoration: InputDecoration(
             hintText: 'search',
             border: InputBorder.none,
             prefixIcon: Icon(
               Icons.search,
               color: Colors.black,
-            )),controller: _searchController,
+            )),
+        controller: _searchController,
       ),
     );
   }
