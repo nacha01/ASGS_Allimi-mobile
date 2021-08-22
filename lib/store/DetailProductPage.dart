4db321230e6e2db02fb257213229bfb0e13ed32b
SeungGun <saverrw2864@naver.com>
2021-08-22 22:18:19 +0900

    Expanded 위젯 이용해서 하단 고정 버튼 생성

--- lib/store/DetailProductPage.dart
+++ lib/store/DetailProductPage.dart
@@ -11,17 +11,53 @@ class DetailProductPage extends StatefulWidget {
 class _DetailProductPageState extends State<DetailProductPage> {
   @override
   Widget build(BuildContext context) {
+    final Size size = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
         backgroundColor: Colors.white,
         leading: IconButton(
-          icon: Icon(Icons.clear,color: Colors.black,),
+          icon: Icon(
+            Icons.clear,
+            color: Colors.black,
+          ),
           onPressed: () => Navigator.pop(context),
         ),
       ),
-      body: Center(
-        child: Text('PRODUCT ID : ${widget.product.prodID}'),
+      body: Column(
+        children: [
+          Expanded(
+            child: SingleChildScrollView(
+              child: Column(
+                children: [
+                  SizedBox(height: size.height * 0.02,),
+                  Container(
+                      width: size.width * 0.95,
+                      height: size.height * 0.35,
+                      child: Image.network(
+                        widget.product.imgUrl1,
+                        fit: BoxFit.fill,
+                      )),
+                  Text('${widget.product.prodName}',textScaleFactor: 3,),
+                  Text('${widget.product.prodInfo}',textScaleFactor: 3),
+                  Text('${widget.product.prodName}',textScaleFactor: 3,),
+                  Text('${widget.product.prodInfo}',textScaleFactor: 3),
+                  Text('${widget.product.prodName}',textScaleFactor: 3,),
+                  Text('${widget.product.prodInfo}',textScaleFactor: 3),
+                  Text('${widget.product.prodName}',textScaleFactor: 3,),
+                  Text('${widget.product.prodInfo}',textScaleFactor: 3),
+                  Text('${widget.product.prodName}',textScaleFactor: 3,),
+                  Text('${widget.product.prodInfo}',textScaleFactor: 3),
+                  Text('${widget.product.prodName}',textScaleFactor: 3,),
+                  Text('${widget.product.prodInfo}',textScaleFactor: 3),
+
+                ],
+              ),
+            ),
+          ),
+          Container(color: Colors.blue, width: size.width, height: size.height * 0.15,)
+
+        ],
       ),
     );
   }
