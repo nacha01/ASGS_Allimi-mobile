41563e30506223a98906be75fa60b4b541160d17
nacha <asgshighschool@gmail.com>
2022-02-06 00:13:06 +0900

    1. 웹뷰 호출 initialUrlRequest 속성 사용 대신 웹뷰 컨트롤러로 postUrl 로 호출 2. ios 전용 앱 링크, 마켓 링크 가져오는 함수 추가 3. 웹뷰내의 모든 콜백함수에 플랫폼 구분 4. ios 에서의 결제 앱 및 마켓 호출 코드 추

--- lib/store/PaymentWebViewPage.dart
+++ lib/store/PaymentWebViewPage.dart
@@ -14,6 +14,7 @@ class PaymentWebViewPage extends StatefulWidget {
   final int totalPrice;
   final String productName;
   final String oID;
+
   PaymentWebViewPage({this.totalPrice, this.productName, this.oID});
 
   @override
@@ -45,7 +46,7 @@ class _PaymentWebViewPageState extends State<PaymentWebViewPage> {
         appScheme != 'about';
   }
 
-  Future<String> getAppUrl(String url) async {
+  Future<String> getAppUrlForAndroid(String url) async {
     if (Platform.isAndroid) {
       return await platform
           .invokeMethod('getAppUrl', <String, Object>{'url': url});
@@ -54,12 +55,57 @@ class _PaymentWebViewPageState extends State<PaymentWebViewPage> {
     }
   }
 
-  Future<String> getMarketUrl(String url) async {
+  Future<String> getMarketUrlForAndroid(String url) async {
     if (Platform.isAndroid) {
       return await platform
           .invokeMethod('getMarketUrl', <String, Object>{'url': url});
     } else {
       return url;
+    }
+  }
+
+  String getAppUrlForIOS(String url) {
+    return url;
+  }
+
+  String getMarketUrlForIOS(String url) {
+    switch (Uri.parse(url).scheme) {
+      case 'kftc-bankpay': // 뱅크페이
+        return 'https://itunes.apple.com/kr/app/id398456030';
+      case 'ispmobile': // ISP/페이북
+        return 'https://itunes.apple.com/kr/app/id369125087';
+      case 'hdcardappcardansimclick': // 현대카드 앱카드
+        return 'https://itunes.apple.com/kr/app/id702653088';
+      case 'shinhan-sr-ansimclick': // 신한 앱카드
+        return 'https://itunes.apple.com/app/id572462317';
+      case 'kb-acp': // KB국민 앱카드
+        return 'https://itunes.apple.com/kr/app/id695436326';
+      case 'mpocket.online.ansimclick': // 삼성앱카드
+        return 'https://itunes.apple.com/kr/app/id535125356';
+      case 'lottesmartpay': // 롯데 모바일결제
+        return 'https://itunes.apple.com/kr/app/id668497947';
+      case 'lotteappcard': // 롯데 앱카드
+        return 'https://itunes.apple.com/kr/app/id688047200';
+      case 'cloudpay': // 하나1Q페이(앱카드)
+        return 'https://itunes.apple.com/kr/app/id847268987';
+      case 'citimobileapp': // 시티은행 앱카드
+        return 'https://itunes.apple.com/kr/app/id1179759666';
+      case 'payco': // 페이코
+        return 'https://itunes.apple.com/kr/app/id924292102';
+      case 'kakaotalk': // 카카오톡
+        return 'https://itunes.apple.com/kr/app/id362057947';
+      case 'lpayapp': // 롯데 L.pay
+        return 'https://itunes.apple.com/kr/app/id1036098908';
+      case 'wooripay': // 우리페이
+        return 'https://itunes.apple.com/kr/app/id1201113419';
+      case 'nhallonepayansimclick': // NH농협카드 올원페이(앱카드)
+        return 'https://itunes.apple.com/kr/app/id1177889176';
+      case 'hanawalletmembers': // 하나카드(하나멤버스 월렛)
+        return 'https://itunes.apple.com/kr/app/id1038288833';
+      case 'shinsegaeeasypayment': // 신세계 SSGPAY
+        return 'https://itunes.apple.com/app/id666237916';
+      default:
+        return url;
     }
   }
 
@@ -92,39 +138,42 @@ class _PaymentWebViewPageState extends State<PaymentWebViewPage> {
                 color: Colors.black,
               ))),
       body: InAppWebView(
-        initialUrlRequest: URLRequest(
-            url: Uri.parse('https://web.nicepay.co.kr/v3/v3Payment.jsp'),
-            method: 'POST',
-            body: Uint8List.fromList(
-                cp949.encode('GoodsName=${widget.productName}&'
-                    'Amt=${widget.totalPrice}&'
-                    'MID=$_MID&'
-                    'ReturnURL=$_RETURN_URL&'
-                    'EdiDate=$_ediDate&'
-                    'Moid=${widget.oID}&'
-                    'SignData=${_getSignData()}&'
-                    'CharSet=euc-kr'))),
-        onLoadStart: (controller, uri) {
-          if (uri.scheme == 'about') {
-            _inAppWebViewController.goBack();
-          }
-          if (uri.scheme == 'intent') {
-            var url = uri.toString();
-            _inAppWebViewController.stopLoading();
-            getAppUrl(url).then((value) async {
-              if (await canLaunch(value)) {
-                await launch(value);
+        onLoadStart: (controller, uri) async {
+          if (Platform.isAndroid) {
+            if (uri.scheme == 'about') {
+              _inAppWebViewController.goBack();
+            }
+            if (uri.scheme == 'intent') {
+              var url = uri.toString();
+              _inAppWebViewController.stopLoading();
+              getAppUrlForAndroid(url).then((value) async {
+                if (await canLaunch(value)) {
+                  await launch(value);
+                } else {
+                  final market = await getMarketUrlForAndroid(url);
+                  await launch(market);
+                }
+              });
+            }
+          } else if (Platform.isIOS) {
+            if (_isAppLink(uri.toString())) {
+              if (await canLaunch(uri.toString())) {
+                await launch(getAppUrlForIOS(uri.toString()),
+                    forceSafariVC: false);
               } else {
-                final market = await getMarketUrl(url);
-                await launch(market);
+                var marketUrl = getMarketUrlForIOS(uri.toString());
+                await launch(marketUrl, forceSafariVC: false);
               }
-            });
+            }
           }
         },
         shouldOverrideUrlLoading: (controller, request) async {
           var uri = await controller.getUrl();
-          if (uri != null && uri.scheme == 'intent') {
-            return NavigationActionPolicy.CANCEL;
+          if (Platform.isAndroid) {
+            if (uri != null && uri.scheme == 'intent') {
+              return NavigationActionPolicy.CANCEL;
+            }
+            return NavigationActionPolicy.ALLOW;
           }
           return NavigationActionPolicy.ALLOW;
         },
@@ -143,6 +192,17 @@ class _PaymentWebViewPageState extends State<PaymentWebViewPage> {
                           AndroidMixedContentMode.MIXED_CONTENT_ALWAYS_ALLOW,
                       thirdPartyCookiesEnabled: true,
                       cacheMode: AndroidCacheMode.LOAD_DEFAULT)));
+          _inAppWebViewController.postUrl(
+              url: Uri.parse('https://web.nicepay.co.kr/v3/v3Payment.jsp'),
+              postData: Uint8List.fromList(
+                  cp949.encode('GoodsName=${widget.productName}&'
+                      'Amt=${widget.totalPrice}&'
+                      'MID=$_MID&'
+                      'ReturnURL=$_RETURN_URL&'
+                      'EdiDate=$_ediDate&'
+                      'Moid=${widget.oID}&'
+                      'SignData=${_getSignData()}&'
+                      'CharSet=euc-kr')));
         },
       ),
     );
