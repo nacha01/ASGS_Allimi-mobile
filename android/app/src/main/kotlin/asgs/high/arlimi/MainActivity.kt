414444e1d7d385000d7fb6befabb5b5012d85332
SeungGun <saverrw2864@naver.com>
2022-02-05 16:18:46 +0900

    결제 앱 호출을 위한(App Link) intent url을 처리하는 로직을 네이티브 코드(MainActivity.kt)에 추가하여 Method Channel 생성

--- android/app/src/main/kotlin/asgs/high/arlimi/MainActivity.kt
+++ android/app/src/main/kotlin/asgs/high/arlimi/MainActivity.kt
@@ -1,6 +1,44 @@
 package asgs.high.arlimi;
 
+import android.content.Intent
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.embedding.engine.FlutterEngine
+import io.flutter.plugin.common.MethodChannel
 
 class MainActivity: FlutterActivity() {
+    private val CHANNEL = "asgs"
+
+    override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
+        super.configureFlutterEngine(flutterEngine)
+
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
+            call, result ->
+            if(call.method == "getAppUrl"){
+                try{
+                    val url = call.argument<String>("url")
+
+                    val intent = Intent.parseUri(url, Intent.URI_INTENT_SCHEME)
+                    result.success(intent.dataString)
+                }
+                catch (e : Exception){
+                    result.notImplemented()
+                }
+            }
+            else if(call.method == "getMarkerUrl"){
+                try {
+                    val url = call.argument<String>("url");
+                    val intent = Intent.parseUri(url, Intent.URI_INTENT_SCHEME)
+
+                    var packageName = intent.`package`
+                    if(packageName != null){
+                        result.success("market://details?id="+packageName)
+                    }
+                    result.notImplemented()
+                }
+                catch (e : Exception){
+                    result.notImplemented()
+                }
+            }
+        }
+    }
 }
