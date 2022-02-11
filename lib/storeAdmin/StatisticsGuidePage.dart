90480756199f51f2254ef3e267dad057e4f90057
SeungGun <saverrw2864@naver.com>
2022-02-11 15:55:47 +0900

    내용 추가

--- lib/storeAdmin/StatisticsGuidePage.dart
+++ lib/storeAdmin/StatisticsGuidePage.dart
@@ -29,8 +29,9 @@ class _StatisticsGuidePageState extends State<StatisticsGuidePage> {
               ))),
       body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.all(size.width * 0.02),
+          padding: EdgeInsets.all(size.width * 0.03),
           child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Row(
                 children: [
@@ -42,6 +43,12 @@ class _StatisticsGuidePageState extends State<StatisticsGuidePage> {
               ),
               Text(
                   '- 시간 설정의 경우 "설정 없음"의 의미는 00시 00분으로 Default 값으로 사용합니다. (= 오전 12시, 밤 12시)  '),
+              Text('- 주문 상태 값'),
+              Text('미결제 = 0'),
+              Text('결제 완료 = 1'),
+              Text('주문 처리 중 = 2'),
+              Text('수령 완료 = 3'),
+              Text('결제 취소 = 4'),
               SizedBox(
                 height: size.height * 0.025,
               ),
@@ -52,6 +59,7 @@ class _StatisticsGuidePageState extends State<StatisticsGuidePage> {
                           TextStyle(fontSize: 17, fontWeight: FontWeight.bold)),
                 ],
               ),
+              Text('매출 통계의 경우에는 실제 결제가 된 주문에 대해서만 집계합니다. (미결제, 결제 취소는 미포함)'),
               Text(
                   '- 매출 통계에서 "날짜 단위"가 "전체"를 제외한 나머지의 경우에는 시간 설정하지 않을 것을 권고합니다.'),
               Text(
@@ -129,6 +137,28 @@ class _StatisticsGuidePageState extends State<StatisticsGuidePage> {
                 ],
               ),
               Text('- 시작 날짜와 시작 시간부터 종료 날짜와 종료 시간까지의 총 매출을 보여줍니다. '),
+              SizedBox(
+                height: size.height * 0.025,
+              ),
+              Divider(),
+              Row(
+                children: [
+                  Text('상품 통계',
+                      style:
+                          TextStyle(fontSize: 17, fontWeight: FontWeight.bold)),
+                ],
+              ),
+              Text('- 상품 통계는 각 상품의 구매수와 예약수를 동시에 보여줍니다.'),
+              Text('- 그래서 "구매 설정"과 "예약 설정" 옵션 값은 각각 하나이상은 선택되어야 합니다. '),
+              Text('- 큰 부류로는 "미결제"와 "결제"로 나뉘어서 보여줍니다. '),
+              Text('- 결제가 된 상품 통계의 경우에는 3가지의 중복 가능한 옵션들이 존재합니다.'),
+              Text('- 선택지가 많을수록 조회하고자 하는 기준 값이 늘어나게 됩니다.'),
+              Text('ex) "결제 완료" 만 보고 싶다면 "결제 완료"에만 체크',
+                  style: TextStyle(color: Colors.grey, fontSize: 13)),
+              Text('결제 완료된 것과 수령 완료된 상품 통계를 원하면 "결제 완료"와 "수령 완료" 옵션에 체크',
+                  style: TextStyle(color: Colors.grey, fontSize: 13)),
+              Text('결제된 모든 상품들을 보고 싶다면 3가지의 조건 모두 체크',
+                  style: TextStyle(color: Colors.grey, fontSize: 13))
             ],
           ),
         ),
