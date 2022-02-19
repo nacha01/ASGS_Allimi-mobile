a079ea56a13e2d0502d51a485d9e807a663aaa45
SeungGun <saverrw2864@naver.com>
2022-02-19 17:59:40 +0900

    공통되는 신분 핸들링을 위한 클래스 추가

---
+++ lib/data/status_data.dart
@@ -0,0 +1,5 @@
+class Status {
+  // UI에 보여줄 "identity" 속성 값에 대해 mapping 을 위한 클래스
+  static final statusList = ['재학생', '학부모', '교사', '졸업생', '기타'];
+  static final statusMap = {'재학생': 1, '학부모': 2, '교사': 3, '졸업생': 4, '기타': 5};
+}
