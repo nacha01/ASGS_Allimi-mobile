6f97725ce375ee0561ec8302b3aa8e66fac94ad3
SeungGun <saverrw2864@naver.com>
2022-02-19 21:48:17 +0900

    카테고리 이름 임시 롤백

--- lib/data/category_data.dart
+++ lib/data/category_data.dart
@@ -1,30 +1,30 @@
 class Category {
   // UI에 보여줄 상품의 "category" 속성 값에 대해 mapping 을 위한 클래스
-  static final c1 = '음료';
-  static final c2 = '과자';
-  static final c3 = '아이스크림';
-  static final c4 = '커피';
-  static final c5 = '생필품';
+  static final c1 = '음식';
+  static final c2 = '간식';
+  static final c3 = '음료';
+  static final c4 = '문구';
+  static final c5 = '핸드메이드';
   static final categoryIndexToStringMap = {
-    0: '$c1류',
-    1: '$c2류',
-    2: '$c3류',
-    3: '$c4류',
-    4: '$c5류'
+    0: '$c1',
+    1: '$c2',
+    2: '$c3',
+    3: '$c4',
+    4: '$c5'
   };
   static final categoryStringToIndexMap = {
-    '$c1류': 0,
-    '$c2류': 1,
-    '$c3류': 2,
-    '$c4류': 3,
-    '$c5류': 4
+    '$c1': 0,
+    '$c2': 1,
+    '$c3': 2,
+    '$c4': 3,
+    '$c5': 4
   };
-  static final categoryList = ['$c1류', '$c2류', '$c3류', '$c4류', '$c5류'];
+  static final categoryList = ['$c1', '$c2', '$c3', '$c4', '$c5'];
   static final categoryImageNamePrefixMap = {
-    '$c1류': 'F',
-    '$c2류': 'S',
-    '$c3류': 'D',
-    '$c4류': 'SS',
-    '$c5류': 'H'
+    '$c1': 'F',
+    '$c2': 'S',
+    '$c3': 'D',
+    '$c4': 'SS',
+    '$c5': 'H'
   };
 }
