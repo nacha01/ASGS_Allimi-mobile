96a5d21b0ce6aa0c0deb1e835e61c26201ea1e4f
SeungGun <saverrw2864@naver.com>
2022-07-03 21:54:37 +0900

    modify: 모바일 학생증 페이지에의 상단 바 테마 변경 및 코드 정리

--- lib/store/MobileStudentCard.dart
+++ lib/store/MobileStudentCard.dart
@@ -14,7 +14,6 @@ class MobileStudentCard extends StatefulWidget {
 }
 
 class _MobileStudentCardState extends State<MobileStudentCard> {
-
   void onChanged(value) {
     print(value);
     setState(() {
@@ -27,83 +26,83 @@ class _MobileStudentCardState extends State<MobileStudentCard> {
     print(widget.user.uid);
     final size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar : AppBar(
-        title : Text('Mobile Student Card')
-      ),
-      body : Padding(
-        padding: EdgeInsets.all(size.width * 0.02),
-        child: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children : [
-                  Text(
-                    '사용자 아이디',
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 20.0
-                    ),
-                  ) ,
-                  Text(
-                    '${ widget.user.uid }',
-                    style: TextStyle(
-                      fontSize: 20.0
-                    ),
-                  )
-              ]),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: [
-                  Text(
-                    '이름',
-                    style: TextStyle(
-                      fontWeight: FontWeight.bold,
-                      fontSize: 20.0
-                    ),
-                  ),
-                  Text(
-                    '${widget.user.name}',
-                    style: TextStyle(
-                        fontSize: 20.0
-                    ),
-                  )
-                ],
-              ),
-              Divider(
-                thickness: 2.0,
-              ),
-              RadioListTile(
-                  title: const Text('출석'),
-                  value: 'A',
-                  groupValue: widget.prefixId,
-                  onChanged: onChanged
-              ),
-              RadioListTile(
-                  title: const Text('우산'),
-                  value: 'U',
-                  groupValue: widget.prefixId,
-                  onChanged: onChanged
-              ),
-              Divider(
-               thickness: 2.0,
-              ),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                 Center(
-                   child: QrImage(
-                    data: '${widget.prefixId}_${widget.user.uid}',
-                     size: 250,
-                   ),
-                 )
-               ],
-              )
-            ],
+        appBar: AppBar(
+          backgroundColor: Color(0xFF9EE1E5),
+          title: Text(
+            '모바일 학생증',
+            style: TextStyle(color: Colors.black),
           ),
+          centerTitle: true,
+          leading: IconButton(
+              onPressed: () => Navigator.pop(context, true),
+              icon: Icon(
+                Icons.arrow_back,
+                color: Colors.black,
+              )),
         ),
-      )
-    );
+        body: Padding(
+          padding: EdgeInsets.all(size.width * 0.02),
+          child: SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        '사용자 아이디',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 20.0),
+                      ),
+                      Text(
+                        '${widget.user.uid}',
+                        style: TextStyle(fontSize: 20.0),
+                      )
+                    ]),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      '이름',
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold, fontSize: 20.0),
+                    ),
+                    Text(
+                      '${widget.user.name}',
+                      style: TextStyle(fontSize: 20.0),
+                    )
+                  ],
+                ),
+                Divider(
+                  thickness: 2.0,
+                ),
+                RadioListTile(
+                    title: const Text('출석'),
+                    value: 'A',
+                    groupValue: widget.prefixId,
+                    onChanged: onChanged),
+                RadioListTile(
+                    title: const Text('우산'),
+                    value: 'U',
+                    groupValue: widget.prefixId,
+                    onChanged: onChanged),
+                Divider(
+                  thickness: 2.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Center(
+                      child: QrImage(
+                        data: '${widget.prefixId}_${widget.user.uid}',
+                        size: 250,
+                      ),
+                    )
+                  ],
+                )
+              ],
+            ),
+          ),
+        ));
   }
 }
