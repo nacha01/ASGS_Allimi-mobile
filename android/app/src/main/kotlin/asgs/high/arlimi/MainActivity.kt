f1a2c13b2d92afee363f8184f93b3e8916f8bf3f
SeungGun <saverrw2864@naver.com>
2023-02-11 01:59:48 +0900

    refactor: 채널 메소드 이름 변경

--- android/app/src/main/kotlin/asgs/high/arlimi/MainActivity.kt
+++ android/app/src/main/kotlin/asgs/high/arlimi/MainActivity.kt
@@ -5,37 +5,32 @@ import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.engine.FlutterEngine
 import io.flutter.plugin.common.MethodChannel
 
-class MainActivity: FlutterActivity() {
+class MainActivity : FlutterActivity() {
     private val CHANNEL = "asgs"
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
 
-        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler {
-            call, result ->
-            if(call.method == "getAppUrl"){
-                try{
+        MethodChannel(flutterEngine.dartExecutor.binaryMessenger, CHANNEL).setMethodCallHandler { call, result ->
+            if (call.method == "getAppUrlForAndroid") {
+                try {
                     val url = call.argument<String>("url")
-
                     val intent = Intent.parseUri(url, Intent.URI_INTENT_SCHEME)
                     result.success(intent.dataString)
-                }
-                catch (e : Exception){
+                } catch (e: Exception) {
                     result.notImplemented()
                 }
-            }
-            else if(call.method == "getMarkerUrl"){
+            } else if (call.method == "getMarketUrlForAndroid") {
                 try {
                     val url = call.argument<String>("url");
                     val intent = Intent.parseUri(url, Intent.URI_INTENT_SCHEME)
 
                     var packageName = intent.`package`
-                    if(packageName != null){
-                        result.success("market://details?id="+packageName)
+                    if (packageName != null) {
+                        result.success("market://details?id=" + packageName)
                     }
                     result.notImplemented()
-                }
-                catch (e : Exception){
+                } catch (e: Exception) {
                     result.notImplemented()
                 }
             }
