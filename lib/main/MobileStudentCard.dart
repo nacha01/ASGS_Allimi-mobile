0a2d745f5c76b24bbfc6a184eda8aa534c775c9d
SeungGun <saverrw2864@naver.com>
2024-03-15 00:07:27 +0900

    refactor: 모바일 학생증 페이지 리팩토링

--- lib/main/MobileStudentCard.dart
+++ lib/main/MobileStudentCard.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 import 'package:qr_flutter/qr_flutter.dart';
 import 'package:asgshighschool/data/user.dart';
 
+enum StudentCardPurpose { ATTENDANCE, UMBRELLA }
+
 class MobileStudentCard extends StatefulWidget {
   MobileStudentCard({this.user});
 
@@ -13,13 +15,21 @@ class MobileStudentCard extends StatefulWidget {
 }
 
 class _MobileStudentCardState extends State<MobileStudentCard> {
-  String? _prefixId = 'A';
+  StudentCardPurpose _prefixId = StudentCardPurpose.ATTENDANCE;
+  late List<bool> _isSelected;
+  bool _isAttendance = true;
+  bool _isUmbrella = false;
 
-  void onChanged(value) {
-   // print(value);
+  void onChanged(StudentCardPurpose? purpose) {
     setState(() {
-      _prefixId = value;
+      _prefixId = purpose ?? StudentCardPurpose.ATTENDANCE;
     });
+  }
+
+  @override
+  void initState() {
+    _isSelected = [_isAttendance, _isUmbrella];
+    super.initState();
   }
 
   @override
@@ -29,66 +39,117 @@ class _MobileStudentCardState extends State<MobileStudentCard> {
         appBar: ThemeAppBar(barTitle: '모바일 학생증'),
         body: Padding(
           padding: EdgeInsets.all(size.width * 0.02),
-          child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text('사용자 정보',
+                  style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold)),
+              Divider(),
+              SizedBox(
+                height: size.height * 0.01,
+              ),
+              Row(
+                children: [
+                  Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
                       Text(
-                        '사용자 아이디',
+                        '아이디: ',
                         style: TextStyle(
-                            fontWeight: FontWeight.bold, fontSize: 20.0),
+                          fontWeight: FontWeight.bold,
+                          fontSize: 16.0,
+                        ),
+                        textAlign: TextAlign.left,
                       ),
                       Text(
-                        '${widget.user!.uid}',
-                        style: TextStyle(fontSize: 20.0),
-                      )
-                    ]),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  children: [
-                    Text(
-                      '이름',
-                      style: TextStyle(
-                          fontWeight: FontWeight.bold, fontSize: 20.0),
-                    ),
-                    Text(
-                      '${widget.user!.name}',
-                      style: TextStyle(fontSize: 20.0),
-                    )
-                  ],
-                ),
-                Divider(
-                  thickness: 2.0,
-                ),
-                RadioListTile(
-                    title: const Text('출석'),
-                    value: 'A',
-                    groupValue: _prefixId,
-                    onChanged: onChanged),
-                RadioListTile(
-                    title: const Text('우산'),
-                    value: 'U',
-                    groupValue: _prefixId,
-                    onChanged: onChanged),
-                Divider(
-                  thickness: 2.0,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Center(
-                      child: QrImage(
-                        data: '${_prefixId}_${widget.user!.uid}',
-                        size: 250,
+                        '이름: ',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16.0),
+                        textAlign: TextAlign.left,
                       ),
-                    )
-                  ],
-                )
-              ],
-            ),
+                      Text(
+                        '학번: ',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold, fontSize: 16.0),
+                        textAlign: TextAlign.left,
+                      ),
+                    ],
+                  ),
+                  SizedBox(
+                    width: size.width * 0.02,
+                  ),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          '${widget.user!.uid}',
+                          style: TextStyle(fontSize: 16.0),
+                        ),
+                        Text(
+                          '${widget.user!.name}',
+                          style: TextStyle(fontSize: 16.0),
+                        ),
+                        Text(
+                          '${widget.user!.studentId}',
+                          style: TextStyle(fontSize: 16.0),
+                        )
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(
+                height: size.height * 0.02,
+              ),
+              Divider(),
+              Text(
+                '모바일 학생증 QR',
+                style: TextStyle(fontSize: 21, fontWeight: FontWeight.bold),
+              ),
+              SizedBox(
+                height: size.height * 0.01,
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  ToggleButtons(
+                    constraints: BoxConstraints.expand(
+                        width: (size.width / 2) - size.width * 0.05),
+                    children: [Text('출석'), Text('우산')],
+                    isSelected: _isSelected,
+                    onPressed: (index) {
+                      if (index == 0) {
+                        _isAttendance = true;
+                        _isUmbrella = false;
+                        _prefixId = StudentCardPurpose.ATTENDANCE;
+                      } else {
+                        _isAttendance = false;
+                        _isUmbrella = true;
+                        _prefixId = StudentCardPurpose.UMBRELLA;
+                      }
+                      setState(() {
+                        _isSelected = [_isAttendance, _isUmbrella];
+                      });
+                    },
+                  ),
+                ],
+              ),
+              Expanded(
+                child: Center(
+                  child: widget.user!.identity != 1
+                      ? Text(
+                          '재학생이 아닙니다.',
+                          style: TextStyle(fontSize: 16, color: Colors.grey),
+                        )
+                      : QrImage(
+                          data:
+                              '${_prefixId}_${widget.user!.studentId}_${widget.user!.name}',
+                          size: 250,
+                        ),
+                ),
+              )
+            ],
           ),
         ));
   }
